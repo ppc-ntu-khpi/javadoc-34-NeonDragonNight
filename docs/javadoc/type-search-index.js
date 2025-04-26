39ee85ef2dcbdcdad019b166d07691dc5b0c5c2a
Без'язичний ростислав <159633985+NeonDragonNight@users.noreply.github.com>
2025-04-26 12:53:52 +0300

    Delete docs/javadoc directory

--- docs/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"domain","l":"Artist"},{"p":"domain","l":"Editor"},{"p":"domain","l":"Employee"},{"p":"test","l":"EmployeesTest"},{"p":"domain","l":"GraphicIllustrator"},{"p":"domain","l":"Manager"},{"p":"domain","l":"TechnicalWriter"}];updateSearchResults();
